c543b9bba61c11acdff664e95c3b3d32557c1d8a
yousifqasim <yusif@zinzo.com>
2024-04-21 20:42:22 +0300

    Refactor faculty_create.blade.php and subject_create.blade.php to improve form styling and button text

--- resources/views/contents/content_create.blade.php
+++ resources/views/contents/content_create.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="flex items-center justify-center h-screen bg-gray-100">
-        <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-            <h1 class="text-2xl font-bold mb-4">Create New content</h1>
+        <div class="bg-blue-600 p-6 rounded-lg shadow-md w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-4 text-center text-white">Create New content</h1>
+            <h6 class="text-center w-full text-white">Please Fill information below</h6>
 
-            <form action="{!! route('content.store') !!}" method="POST">
+            <form action="{!! route('content.store') !!}" method="POST" class="mt-8 space-y-6">
                 @csrf
 
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700 font-medium mb-2">content:</label>
-                    <input type="text" name="material_covered" id="name"
+                    <input type="text" name="material_covered" id="name" placeholder="please enter material covered"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('material_covered')
@@ -19,8 +19,7 @@
 
 
                 <div class="mb-4">
-                    <label for="faculty_id" class="block text-gray-700 font-medium mb-2">Subject:</label>
-                    <select name="subject_id" id="faculty_id"
+                    <select name="subject_id" id="faculty_id" required placeholder="please select subject"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500">
 
                         @foreach($subjects as $subject)
@@ -31,10 +30,12 @@
                     <p class="text-red-500 text-sm">{{ $message }}</p>
                     @enderror
                 </div>
+                <div class="flex justify-center">
 
-                <button type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">Create
-                    Content</button>
+<button type="submit"
+    class="text-white border border-white  hover:bg-white hover:text-blue-600 font-bold py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline">Create
+    Content</button>
+  </div>
             </form>
         </div>
     </div>
--- resources/views/subjects/subject_create.blade.php
+++ resources/views/subjects/subject_create.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="flex items-center justify-center h-screen bg-gray-100">
-        <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-            <h1 class="text-2xl font-bold mb-4">Create New Subject</h1>
+        <div class="bg-blue-600 p-6 rounded-lg shadow-md w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-4 text-center text-white">Create New Subject</h1>
+            <h6 class="text-center w-full text-white">Please Fill information below</h6>
 
-            <form action="{!! route('subject.store') !!}" method="POST">
+            <form action="{!! route('subject.store') !!}" method="POST" class="mt-8 space-y-6">
                 @csrf
 
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700 font-medium mb-2">Name:</label>
-                    <input type="text" name="name" id="name"
+                    <input type="text" name="name" id="name" placeholder="please enter name"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('name')
@@ -17,8 +17,7 @@
                 </div>
 
                 <div class="mb-4">
-                    <label for="code" class="block text-gray-700 font-medium mb-2">code:</label>
-                    <input type="text" name="code" id="code"
+                    <input type="text" name="code" id="code" placeholder="please enter code"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('code')
@@ -27,10 +26,11 @@
                 </div>
 
 
+                <div class="flex justify-center">
 
                 <button type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">Create
-                    subject</button>
+                    class="text-white border border-white  hover:bg-white hover:text-blue-600 font-bold py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline">Create Subject</button>
+                  </div>
             </form>
         </div>
     </div>
