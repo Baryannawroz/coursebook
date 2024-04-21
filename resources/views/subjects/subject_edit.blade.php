d58bb62e658ea516dbfbe3ce7a0ce5c3ed33ab22
yousifqasim <yusif@zinzo.com>
2024-04-21 20:48:35 +0300

    Refactor subject_edit.blade.php to improve form styling and button text

--- resources/views/subjects/subject_edit.blade.php
+++ resources/views/subjects/subject_edit.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     <div class="flex items-center justify-center h-screen bg-gray-100">
-        <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-            <h1 class="text-2xl font-bold mb-4">update New Subject</h1>
+        <div class="bg-blue-600 p-6 rounded-lg shadow-md w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-4 text-center text-white">update New Subject</h1>
 
-            <form action="{!! route('subject.update',$subject->id) !!}" method="POST">
+            <form action="{!! route('subject.update',$subject->id) !!}" method="POST" class="mt-8 space-y-6">
                 @csrf
 
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700 font-medium mb-2">Name:</label>
-                    <input type="text" name="name" id="name" value="{{ $subject->name }}"
+                    <input type="text" name="name" id="name" value="{{ $subject->name }}" placeholder="please enter name"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('name')
@@ -17,8 +16,7 @@
                 </div>
 
                 <div class="mb-4">
-                    <label for="code" class="block text-gray-700 font-medium mb-2">code:</label>
-                    <input type="text" name="code" id="code" value="{{ $subject->code }}"
+                    <input type="text" name="code" id="code" value="{{ $subject->code }}" placeholder="please enter code"
                         class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('code')
@@ -28,9 +26,11 @@
 
 
 
-                <button type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">Create
-                    Apartment</button>
+                <div class="flex justify-center">
+
+<button type="submit"
+    class="text-white border border-white  hover:bg-white hover:text-blue-600 font-bold py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline">Update Subject</button>
+  </div>
             </form>
         </div>
     </div>
