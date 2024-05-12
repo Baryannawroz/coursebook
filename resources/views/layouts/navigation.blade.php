3fcd02949caace58f1c113e38d87aadc133eb19e
yousifqasim <yusif@zinzo.com>
2024-05-12 19:06:16 +0300

    Refactor navigation.blade.php for improved styling and accessibility

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
     }
 </style>
 
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 ">
+<nav x-data="{ open: false }" class="bg-white border-b  ">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-20 items-center">
