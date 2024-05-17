051f1ee61f50fcfd902aa2f1593bb7f242e815d3
yousifqasim <yusif@zinzo.com>
2024-05-17 15:18:03 +0300

    Refactor form layouts for content, faculty, and subject creation

--- resources/views/subjects/subject_create.blade.php
+++ resources/views/subjects/subject_create.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-    <div class="flex items-center justify-center h-screen bg-gray-100">
-        <div class="bg-blue-600 p-6 rounded-lg shadow-md w-full max-w-md">
-            <h1 class="text-2xl font-bold mb-4 text-center text-white">Create New Subject</h1>
-            <h6 class="text-center w-full text-white">Please Fill information below</h6>
-
-            <form action="{!! route('subject.store') !!}" method="POST" class="mt-8 space-y-6">
-                @csrf
+    <div class="flex items-center justify-center h-screen ">
+        <div class="bg-blue-600 p-6 rounded-lg shadow-md w-3/4 h-3/4 flex justify-center items-center">
+          
+          <form action="{!! route('subject.store') !!}" method="POST" class="mt-8 space-y-6 w-3/4 flex-col justify-center items-center">
+            @csrf
+            <h1 class="text-5xl font-bold mb-4 text-center text-white">Create New Subject</h1>
+            <h6 class="text-center text-xl w-full text-white">Please Fill information below</h6>
 
                 <div class="mb-4">
                     <input type="text" name="name" id="name" placeholder="please enter name"
-                        class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+                        class="border border-gray-300 rounded-lg py-4 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('name')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -18,7 +18,7 @@
 
                 <div class="mb-4">
                     <input type="text" name="code" id="code" placeholder="please enter code"
-                        class="border border-gray-300 rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+                        class="border border-gray-300 rounded-lg py-4 px-3 w-full focus:outline-none focus:border-blue-500"
                         required>
                     @error('code')
                     <p class="text-red-500 text-sm">{{ $message }}</p>
